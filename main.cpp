7124a2187f0173e63cbb22a0198a3fe2c58aa964
Владислав <60321109+nondefraudat@users.noreply.github.com>
2023-12-03 01:10:47 +0300

    Get started. SDL create window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <cstdio>
 
-int main() {
+int main(int argc, char* args[]) {
+	const size_t screenWidth = 640;
+	const size_t screenHeight = 480;
 
-	return 0;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return NULL;
+	}
+	
+	SDL_Window* const window = SDL_CreateWindow(
+			"The Window!", screenWidth, screenHeight, NULL);
+	if (!window) {
+		return NULL;
+	}
+
+	SDL_Surface* const surface = SDL_GetWindowSurface(window);
+	if (!surface) {
+		return NULL;
+	}
+
+	SDL_FillSurfaceRect(surface, nullptr,
+			SDL_MapRGB(surface->format, 255, 255, 255));
+
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Event e;
+	bool quit = false;
+	while (quit == false) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_EVENT_QUIT)
+				quit = true;
+		}
+	}
+
+	return NULL;
 }
