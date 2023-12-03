5538cc0dfe79446a32dd7fb845285216c2a95550
Владислав <60321109+nondefraudat@users.noreply.github.com>
2023-12-03 13:09:30 +0300

    Show BMP. Hello world SDL!

--- main.cpp
+++ main.cpp
@@ -1,29 +1,28 @@
 #include <SDL3/SDL.h>
 #include <cstdio>
 
+const size_t screenWidth = 640;
+const size_t screenHeight = 480;
+
+SDL_Window* window = nullptr;
+SDL_Surface* surface = nullptr;
+SDL_Surface* media = nullptr;
+
+bool init();
+bool loadMedia();
+void close();
+
 int main(int argc, char* args[]) {
-	const size_t screenWidth = 640;
-	const size_t screenHeight = 480;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		return NULL;
-	}
-	
-	SDL_Window* const window = SDL_CreateWindow(
-			"The Window!", screenWidth, screenHeight, NULL);
-	if (!window) {
+	if (!init()) {
 		return NULL;
 	}
 
-	SDL_Surface* const surface = SDL_GetWindowSurface(window);
-	if (!surface) {
+	if (!loadMedia()) {
 		return NULL;
 	}
 
-	SDL_FillSurfaceRect(surface, nullptr,
-			SDL_MapRGB(surface->format, 255, 255, 255));
-
-	SDL_UpdateWindowSurface(window);
+	SDL_BlitSurface(::media, NULL, ::surface, NULL);
+	SDL_UpdateWindowSurface(::window);
 
 	SDL_Event e;
 	bool quit = false;
@@ -34,5 +33,35 @@ int main(int argc, char* args[]) {
 		}
 	}
 
+	close();
 	return NULL;
 }
+
+bool init() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return false;
+	}
+
+	::window = SDL_CreateWindow("Hello World!",
+		::screenWidth, ::screenHeight, NULL);
+	if (!window) {
+		return false;
+	}
+
+	::surface = SDL_GetWindowSurface(::window);
+
+	return true;
+}
+
+bool loadMedia() {
+	::media = SDL_LoadBMP("helloworld.bmp");
+	return ::media != nullptr;
+}
+
+void close() {
+	SDL_DestroySurface(::media);
+	SDL_DestroyWindow(::window);
+	SDL_Quit();
+}
+
+
